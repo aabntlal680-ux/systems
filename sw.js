09380c782d7c0902c3cf1374e6010019e8e87d54
aabntlal680-ux <aabntlal680@gmail.com>
2026-07-05 23:05:27 +0300

    Fix fetch event listener syntax in sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,6 @@ self.addEventListener('install', (e) => {
   // يمكن إضافة ملفات للتخزين المؤقت هنا لاحقاً
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (event) => {
   // يضمن هذا الحدث إمكانية تشغيل التطبيق أو الاستجابة لطلبات الشبكة
 });
